96ee7e07c8d4f5f7f24b8908692754e9951b4280
a500573435708 <a500573435708@gmail.com>
2020-02-15 22:08:40 +0100

    Initial commit

---
+++ src/main/java/com/blackhearth/ukl/lesnik/LesnikApplication.java
@@ -0,0 +1,13 @@
+package com.blackhearth.ukl.lesnik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LesnikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LesnikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blackhearth/ukl/lesnik/LesnikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackhearth.ukl.lesnik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LesnikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
